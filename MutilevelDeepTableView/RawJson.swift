3e74135fde4006f061387393edb9808e108b737e
ernie.cheng <ernie.cheng@perficient.com>
2017-07-06 17:19:34 +0800

    init

---
+++ MutilevelDeepTableView/RawJson.swift
@@ -0,0 +1,9 @@
+//
+//  RawJson.swift
+//  MutilevelDeepTableView
+//
+//  Created by ernie.cheng on 7/6/17.
+//  Copyright © 2017 ernie.cheng. All rights reserved.
+//
+
+import Foundation
---
+++ MutilevelDeepTableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MutilevelDeepTableView
+//
+//  Created by ernie.cheng on 7/6/17.
+//  Copyright © 2017 ernie.cheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
